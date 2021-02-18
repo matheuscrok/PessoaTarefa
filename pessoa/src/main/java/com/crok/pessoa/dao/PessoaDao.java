9866831618119e482cdf1460225a569a7608053e
CROK <CROK@DESKTOP-JMC0DR6>
2021-02-18 16:06:34 -0300

    Agora vai

--- pessoa/src/main/java/com/crok/pessoa/dao/PessoaDao.java
+++ pessoa/src/main/java/com/crok/pessoa/dao/PessoaDao.java
@@ -1,5 +1,11 @@
 package com.crok.pessoa.dao;
 
-public interface PessoaDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crok.pessoa.model.Pessoa;
+
+@Repository
+public interface PessoaDao extends JpaRepository<Pessoa, Integer> {
 
 }
--- pessoa/src/main/java/com/crok/pessoa/rest/PessoaRest.java
+++ pessoa/src/main/java/com/crok/pessoa/rest/PessoaRest.java
@@ -1,5 +1,40 @@
 package com.crok.pessoa.rest;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crok.pessoa.dao.PessoaDao;
+import com.crok.pessoa.model.Pessoa;
+
+@RestController
+@RequestMapping("/rest")
 public class PessoaRest {
 
+	@Autowired
+	private PessoaDao pessoaDao;
+	
+	@GetMapping
+	public List<Pessoa> get() {
+		return pessoaDao.findAll();
+	}
+	
+	@PostMapping
+	public void post(@RequestBody Pessoa pessoa) {
+		pessoaDao.save(pessoa);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(Integer id) {
+		pessoaDao.deleteById(id);
+	}
+	
+	
 }
